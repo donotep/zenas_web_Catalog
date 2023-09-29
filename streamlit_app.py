ae658e446926f1e4a2c5f45a71bafba1b163f90e
donotep <48946951+donotep@users.noreply.github.com>
2023-09-29 10:09:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,5 +28,14 @@ option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list
 product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 
 # use the option selected to go back and get all the info from the databaseimport streamlit
-
-streamlit.title("Zena's Amazing Web Catalog")
+my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where
+color_or_style = '" + option + "';")
+df2 = my_cur.fetchone()
+streamlit.image(
+df2[0],
+width=400,
+caption= product_caption
+)
+streamlit.write('Price: ', df2[1])
+streamlit.write('Sizes Available: ',df2[2])
+streamlit.write(df2[3])
